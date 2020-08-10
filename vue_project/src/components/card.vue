fcf34a46f98ae060a86d0a191e85d8562721a5b4
ars1208 <ara.kaz1208@gmail.com>
2020-08-10 15:48:37 +0900

    add change-color logic

--- vue_project/src/components/card.vue
+++ vue_project/src/components/card.vue
@@ -1,26 +1,38 @@
 <template>
-    <div class="card-content">
-        <a href="#">word</a>
+    <div class="card-content" v-on:click="isActivePushed=!isActivePushed" v-bind:class="{pushedColor:isActivePushed}">
+        <p>word</p>
     </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      isActivePushed: false
+    }
+  }
+}
 </script>
 
 <style>
-    .card-content {
-        height: 130px;
-        width: 220px;
-        background-color: #b5b5ac;
-        font-size: 40px;
-        line-height: 130px;
-        text-align: center;
-        border-radius: 20px;
-        cursor: pointer;
-        display: block;
-    }
-
-    a {
-        text-decoration: none;
-    }
+.card-content {
+  height: 130px;
+  width: 220px;
+  background-color: #b5b5ac;
+  font-size: 40px;
+  text-align: center;
+  border-radius: 20px;
+  cursor: pointer;
+  display: block;
+}
+.pushedColor {
+  height: 130px;
+  width: 220px;
+  background-color: blue;
+  font-size: 40px;
+  text-align: center;
+  border-radius: 20px;
+  cursor: pointer;
+  display: block;
+}
 </style>
